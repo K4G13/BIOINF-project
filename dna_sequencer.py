59e5a151673755be481024a0ad2db6fa3e701192
K4G13 <polcyn.piotrek@gmail.com>
2023-05-24 02:44:55 +0200

    v2.0

--- dna_sequencer.py
+++ dna_sequencer.py
@@ -1,17 +1,14 @@
 import sys
+import re
 import numpy as np
 
 def get_data(file_dir=""):
 
-    # get file dir from program arguments
-    if not file_dir and len(sys.argv) >= 2:
-        file_dir = sys.argv[1]
-
     if(file_dir==""):
-        print(f"(!) get_data: no filename entered")
-        exit()
+        print(f"(!) no file input")
+        print(f"(!) using test/test.10-2+2.txt file")
+        file_dir = "test/test.10-2+2.txt"
 
-    # add .txt at the end if there is not one
     if file_dir[-4:] != ".txt":
         file_dir += ".txt"
 
@@ -19,10 +16,24 @@ def get_data(file_dir=""):
         file = open(file_dir, "r")
         data = file.read().split()
         file.close()
-        return data
     except:
-        print(f"(!) get_data: couldn't find file: {file_dir}")
+        print(f"(!) couldn't find file: {file_dir}")
+        print("Exiting...")
         exit()
+
+    temp = file_dir.split(".")[1]
+    negatives = re.findall("\-[0-9]*",temp)
+    positives = re.findall("\+[0-9]*",temp)
+
+    if negatives == []: negatives = 0
+    else: negatives = int(negatives[0][1:])
+
+    if positives == []: positives = 0
+    else: positives = int(positives[0][1:])
+
+    seq_length = int(re.findall(".[0-9]*",temp)[0])
+
+    return data,negatives,positives,seq_length
 
 def coverage(str_a,str_b):
     if str_a == str_b:
@@ -30,7 +41,7 @@ def coverage(str_a,str_b):
     
     for i in range(len(str_a)):
         if str_a[i:] == str_b[:-i]:
-            return len(str_a) - i
+            return int(len(str_a) - i)
     return 0
 
 def create_coverage_matrix(o_list):
@@ -64,42 +75,82 @@ def best_successor(availables,last_el_id,max_score,coverage_matrix):
     else:
         return best
 
-original_set = get_data("test")
-# length of oligonucleotide
-L = len(original_set[0]) 
-# length of sequence
-N = len(original_set) + L - 1
-coverage_matrix = create_coverage_matrix(original_set)
+def summary(resault,og_set):
+    print("Summary:")
+    print(f"Sequence length: \t{resault['seq len']}\t|shoud be: {og_seq_len}")
+    print(f"Positive errors: \t{len(resault['positives'])}\t|shoud be: {og_positives}")
+    print(f"Negative errors: \t{resault['negatives']}\t|shoud be: {og_negatives}")
+    print(f"Elements in sequence: \t{resault['seq no el']}")
 
-output = []
-output_length = 0
-for starting_el in original_set:
+    seq = []
+    for i in resault["seq"]: seq.append(og_set[i])
 
-    # print(f"Starting element: {starting_el}")
+    prev = seq[0]
+    spaces = 0
+    print(seq[0])
+    for el in seq[1:]:
+        spaces += L - coverage(prev,el)
+        for _ in range(spaces): print(" ",end="")
+        print(el)
+        prev = el
 
-    availables_id = list(range(0,len(original_set),1))
-    availables_id.pop(original_set.index(starting_el))
+data_dir = ""
+if len(sys.argv) >= 2: data_dir = sys.argv[1]
+    
+og_set,og_negatives,og_positives,og_seq_len = get_data(data_dir)
+L = len(og_set[0]) # length of oligonucleotide 
 
-    resault = [original_set.index(starting_el)]
+coverage_matrix = create_coverage_matrix(og_set)
+
+best_resault = {
+    "seq" : [], #sequence (indexes)
+    "seq len" : 0, #length of sequence
+    "seq no el": 0, #number of elements in sequence
+    "negatives" : 0, #number of negative errors
+    "positives" : 0 #number of positive errors 
+}
+
+all_elements_in_og_set = len(og_set)
+index_of_iteration = 0
+
+for starting_el_id in range(len(og_set)):
+    print(f"Starting seq: {og_set[starting_el_id]} {round(index_of_iteration/all_elements_in_og_set*100,2)}%     ", end='\r', flush=True)
+    index_of_iteration+=1
+
+    availables = list(range(0,len(og_set),1))
+    availables.pop(starting_el_id)
+
+    resault = [starting_el_id]
     resault_length = L
 
-    while(resault_length<N or availables_id==[]):
+    negative_errors = 0
 
-        max_score = N - resault_length
-        succesor = best_successor(availables_id,resault[-1],max_score,coverage_matrix)
-        if succesor == -1:
-            break
-        resault_length += L - coverage_matrix[resault[-1]][succesor] 
+    while(resault_length < og_seq_len or availables==[]):
+
+        max_score = og_seq_len - resault_length
+        succesor = best_successor(availables,resault[-1],max_score,coverage_matrix)
+
+        if succesor == -1: break
+
+        length_added = L - coverage_matrix[resault[-1]][succesor] 
+        resault_length += length_added
+        if length_added != 1: 
+            negative_errors += length_added - 1
+
         resault.append(succesor)
-        availables_id.pop(availables_id.index(succesor))
+        availables.pop(availables.index(succesor)) #HERE
 
-    if resault_length > output_length:
-        output = resault
-        output_length = resault_length
+    if resault_length >= best_resault["seq len"] and len(resault) > best_resault["seq no el"]:
+        best_resault["seq"] = resault
+        best_resault["seq no el"] = len(resault)
+        best_resault["seq len"] = int(resault_length)
+        best_resault["negatives"] = int(negative_errors)
+        best_resault["positives"] = list(set(list(range(0,len(og_set),1))) - set(resault))
     
-    if resault_length == N:
-        break
+    if resault_length == og_seq_len:
+        break #HERE
+        if best_resault["positives"] == og_positives and best_resault["negatives"] == og_negatives:
+            break
 
-for i in resault:
-    print(original_set[i])
-print(resault_length)
+print()
+summary(best_resault,og_set)
--- download_data.py
+++ download_data.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import os
  
 page = requests.get('https://www.cs.put.poznan.pl/mkasprzak/bio/testy.html')
 soup = BeautifulSoup(page.content, 'html.parser')
 links = soup.select("a")[:-2] #last two links are unimportant
 
+if not os.path.isdir("./data"):
+    os.mkdir("./data")
+
 for link in links:
     datapage = requests.get(str('https://www.cs.put.poznan.pl/mkasprzak/bio/ins/' + links[0].text))
     data = BeautifulSoup(datapage.content, 'html.parser')
-    with open('data/'+link.text, 'w') as f:
+    with open('data/'+link.text+".txt", 'w') as f:
         f.write(data.text)
